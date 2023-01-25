936aeed1ed716e716fe9db4f20565de2ef8a5b27
Alex <avhoi83@gmail.com>
2023-01-24 17:40:26 -0800

    built arm and elevator subsystems and commands

--- src/main/java/frc/robot/commands/rotatingArm/RotateArmToAngle.java
+++ src/main/java/frc/robot/commands/rotatingArm/RotateArmToAngle.java
@@ -1,17 +1,41 @@
 package frc.robot.commands.rotatingArm;
-// package frc.robot.commands;
 
-// import java.util.function.DoubleConsumer;
-// import java.util.function.DoubleSupplier;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
 
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
-// import frc.robot.subsystems.RotatingArm;
+import frc.robot.subsystems.RotatingArm;
+import frc.robot.Constants.ClawConfig;
 
-// public class RotateClawTo extends ProfiledPIDCommand {
+public class RotateArmToAngle extends ProfiledPIDCommand {
 
-//     public RotateClawTo(double targetAngle, RotatingArm arm){
-//         super();
-//     }
+    /**
+     * Rotate the claw arm to a particular angle.
+     * @param targetAngle angle to rotate to above straight down in degrees
+     * @param arm the rotating-arm subsystem
+     */
+    public RotateArmToAngle (double targetAngle, RotatingArm arm){
+        super(
+            new ProfiledPIDController(
+                ClawConfig.kP,
+                ClawConfig.kI,
+                ClawConfig.kD,
+                new Constraints(ClawConfig.maxAcceleration, ClawConfig.maxVelocity)),
+            arm::getAngle,
+            targetAngle,
+            (output, setpoint) -> {arm.rotateClaw(output);},
+            arm
+        );
+
+        getController().setTolerance(0.5, 0.1);
+        addRequirements(arm);
+    }
     
-// }
+    @Override
+    public void initialize() { }
+  
+    @Override
+    public boolean isFinished() {
+      return getController().atSetpoint();
+    }
+}
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,7 +1,41 @@
 package frc.robot.subsystems;
 
+import frc.robot.Constants.ElevatorConfig;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
-    
+  private final CANSparkMax motor = new CANSparkMax(ElevatorConfig.motorPort, MotorType.kBrushless);
+
+  /**
+   * Create a new Elevator.
+   */
+  public Elevator() {
+    motor.restoreFactoryDefaults();
+    motor.getEncoder().setPositionConversionFactor(ElevatorConfig.rotationToDistanceConversion);
+  }
+
+  /** Rotate the arm.
+   * @param speed the speed to rotate at
+   */
+  public void rise(double speed){
+    motor.set(speed);
+  }
+
+  /**
+   * Get the current height of the elevator.
+   */
+  public double getHeight(){
+    return motor.getEncoder().getPosition();
+  }
+
+  /**
+   * Reset the encoder to be at zero height.
+   */
+  public void resetEncoder(){
+    motor.getEncoder().setPosition(0);
+  }
 }
