e17b46e7c3ba0bb0b36d71d71ffdd32ac5b80d3d
LucaHaverty <lucahaverty@gmail.com>
2023-02-12 21:51:12 -0800

    Fixed error in RobotContainer getAutoCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -131,6 +131,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return new ScoreAndBalance(drivetrain, null, null, null);
+    return new ScoreAndBalance(drivetrain, null, null);
   }
 }
