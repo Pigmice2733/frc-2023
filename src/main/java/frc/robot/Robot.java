59ed9eeb6d572c77816b0edda7b13bb7823161fb
PepperLola <julian.orion.wright@gmail.com>
2023-03-24 17:59:50 -0700

    reset arm angle on auto init but not teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -103,8 +103,8 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    m_robotContainer.arm.resetSetpointAndGoal();
-    m_robotContainer.arm.resetEncoder();
+    // m_robotContainer.arm.resetSetpointAndGoal();
+    // m_robotContainer.arm.resetEncoder();
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
